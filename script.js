d16d1d468dd97f982223a910e39cd065c502e021
asouma234 <assmaeslitinealaoui@gmail.com>
2025-06-01 19:14:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("windows").onclick = function () {
+    window.location.href = "windows.html";
+};
+document.getElementById("cloud").onclick = function () {
+    window.location.href = "cloud.html";
+};
+document.getElementById("ia").onclick = function () {
+    window.location.href = "ia.html";
+};
